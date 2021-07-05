912f31b862fff550f9be22764f028fad7e989f79
tin cong phan <tincongphan@gmail.com>
2021-07-05 16:08:05 +0700

    Response Token when SignIn Success by jsonwebtoken

--- controllers/sign_inController/sign_inController.js
+++ controllers/sign_inController/sign_inController.js
@@ -1,16 +1,31 @@
 
 const { User } = require("../../models/index.js")
 const bcryptjs = require('bcryptjs');
+const jwt = require("jsonwebtoken")
 
 const sign_inController = async (req, res) => {
     const { email, password } = req.body;
     try {
         const userSignin = await User.findOne({ where: { email } })
         if (userSignin) {
+
             // If userSignin exist , check password
             const isAuth = bcryptjs.compareSync(password, userSignin.password)
-            if(isAuth){
-                res.status(200).send("SignIn success")
+            if (isAuth) {
+
+                /* Create a Token
+                Token include 2 parameters: payload & secretKey.
+                Payload shouldn'd include password. Because it's not security and can veriry
+                */
+                const payload = {
+                    id: userSignin.id,
+                    email: userSignin.email,
+                    role: userSignin.role
+                }
+                const secretKey = "benxemiendong"
+                const token = jwt.sign(payload, secretKey)
+                res.status(200).send({ messege: "SignIn success", token })
+
             } else {
                 res.status(400).send("Password invalid")
             }
